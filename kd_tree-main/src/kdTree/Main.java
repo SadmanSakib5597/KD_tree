466cf269ac5f37e17385a97d5ab77102d475fe74
Sadman Sakib <46521025+SadmanSakib5597@users.noreply.github.com>
2021-09-05 00:20:32 +0600

    k-dimensional tree

---
+++ kd_tree-main/src/kdTree/Main.java
@@ -0,0 +1,29 @@
+package kdTree;
+
+public class Main {
+    public static void main(String[] args){
+
+        ReadDataFromTextFile readData = new ReadDataFromTextFile("points.txt");
+        readData.read();
+        int [][] arrayOfPoint = readData.getArrayOfPoint();
+        int dimension = readData.getDimension();
+        int numberOfPoint = readData.getNumberOfPoint();
+
+        System.out.println("dimension : " + dimension);
+        System.out.println("number of point : " + numberOfPoint + "\n");
+
+        Operation operation = new Operation(arrayOfPoint, dimension, numberOfPoint);
+        operation.insert();
+        System.out.println("Pre-order print : \n");
+        operation.printTreeInPreOrder(operation.getRoot());
+        System.out.println();
+
+        int[] point = {0,0};
+      //  int[] point1 = {4,0};
+        operation.search(point);
+    //    operation.search(point1);
+
+
+
+    }
+}
